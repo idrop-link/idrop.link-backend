43c7dd9f66423a02832ffa89d48faaed7807c2a7
Christian Schulze <christian.schulze@mni.thm.de>
2015-04-07 21:24:43 +0200

    added first version of documentation

---
+++ doc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "idrop.link-backend",
+  "version": "0.0.0",
+  "description": " API for the idrop.link project",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-04-07T19:23:08.112Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.1"
+  }
+});
