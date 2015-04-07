2b4adf0b3355090de5516eb8acb7d19699d4c78f
Christian Schulze <christian.schulze@mni.thm.de>
2015-04-07 18:18:07 +0200

    added methods and multiple date fields to the token model

--- models/token.js
+++ models/token.js
@@ -9,9 +9,37 @@
         creation_date: {
             type: Date,
             default: Date.now
-        }
+        },
+        expiration_date: Date,
+        invalidation_date: Date
     });
 
+    /* methods */
+    /**
+     * Checks whether or not the token is expired or is valid.
+     *
+     * @return whether or not token is valid
+     */
+    TokenSchema.methods.isExpired = function() {
+        var now = new Date();
+
+        if (this.expiration_date === null)
+            return false;
+
+        if (this.expiration_date >= now.getTime())
+            return true;
+        else
+            return false;
+    };
+
+    /**
+     * Invalidates this very token.
+     */
+    TokenSchema.methods.invalidate = function() {
+        this.invalidation_date = Date().getTime();
+    }
+
     var Token = mongoose.model('Token', TokenSchema);
-    module.exports = Token;
+    module.exports.Token = Token;
+    module.exports.TokenSchema = TokenSchema;
 })();
