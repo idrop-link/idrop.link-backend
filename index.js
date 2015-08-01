2b9de40069b5c01bed88cc5cdc0cfcb2d3148142
Christian Schulze <christian.schulze@mni.thm.de>
2015-08-01 10:49:01 +0200

    improved connection handling and graceful shutdown; hopefully improves heroku SIGTERM signal

--- index.js
+++ index.js
@@ -39,7 +39,11 @@
 
     if (config.logging.do_log) {
         if (config.logging.access_log_path !== '') {
-            var accessLogStream = fs.createWriteStream(path.join(__dirname, config.logging.access_log_path), {flags: 'a'});
+            var accessLogStream = fs.createWriteStream(path.join(__dirname,
+                    config.logging.access_log_path),
+                    {
+                        flags: 'a'
+                    });
             app.use(morgan('combined', {stream: accessLogStream}));
         } else {
             app.use(morgan('combined'));
@@ -62,30 +66,54 @@
     app.use(frontend);
 
     /* Mongoose */
+    var mongooseIsConnected = false;
+
     /* Establish database connection: either use the specified `DB_URI=ADDRESS_TO_MONGODB`
     via the environment, or fall back to the default path */
-    var database = process.env.MONGODB_URI || 'localhost:27017/idroplink';
-    mongoose.connect(database);
+    var connectionFn = function() {
+        var database = process.env.MONGODB_URI || 'localhost:27017/idroplink';
+        mongoose.connect(database);
+    };
+
+    // connect
+    connectionFn();
 
     mongoose.connection.on('connected', function() {
         console.log('Mongoose connected to database');
+        mongooseIsConnected = true;
     });
 
     mongoose.connection.on('error', function(err) {
         console.error(err);
+
+        // closing via driver
+        mongoose.connection.db.close();
+
+        // retry connection until succeeding
+        connectionFn();
     });
 
     mongoose.connection.on('disconnected', function() {
         console.log('Mongoose disconnected');
+        mongooseIsConnected = false;
     });
 
-    /* Mongoose disconnection on app termination */
-    process.on('SIGINT', function() {
-        mongoose.connection.close(function() {
-            console.log('Mongoose is disconnecting due to app termination');
+    var handleDisconnect = function() {
+        console.log('attempting graceful shutdown.');
+
+        if (mongooseIsConnected) {
+            mongoose.connection.close(function() {
+                console.log('Mongoose is disconnecting due to app termination');
+                process.exit(0);
+            });
+        } else {
             process.exit(0);
-        });
-    });
+        }
+    };
+
+    /* Mongoose disconnection on app termination */
+    process.on('SIGINT', handleDisconnect);
+    process.on('SIGTERM', handleDisconnect);
 
     var port = process.env.PORT || config.api.port;
     app.listen(port);
