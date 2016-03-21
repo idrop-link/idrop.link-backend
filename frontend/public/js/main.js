2a4c08fcb9de713a2f22da9ba8280cc764415ed8
Christian Schulze <christian.schulze@mni.thm.de>
2016-03-21 15:55:33 +0100

    DRY'ed sign in; implemented signing in after signing up; closes #55

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -1,6 +1,16 @@
 (function() {
     'use strict';
 
+    function signIn(data, _success, _error) {
+        $.ajax({
+            url: '/signin',
+            method: 'POST',
+            data: data,
+            success: _success,
+            error: _error
+        });
+    }
+
     $(document).ready(function() {
         $('.signup-button').on('click', function(e) {
             var $target = $(e.target);
@@ -13,9 +23,16 @@
                 method: 'POST',
                 data: $form.serialize(),
                 success: function(data) {
-                    window.location = '/welcome?referal=signup';
-                },
-                error: function(res) {
+                    signIn($form.serialize(), function(data) {
+                        window.location = '/welcome?referal=signup';
+                    }, function(res) {
+                        errors.html($.parseJSON(res.responseText).message);
+
+                        if (!errors.hasClass('active')) {
+                            errors.addClass('active');
+                        }
+                    });
+                }, error: function(res) {
                     errors.html($.parseJSON(res.responseText).message);
 
                     if (!errors.hasClass('active')) {
@@ -32,20 +49,16 @@
             var $target = $(e.target);
             var $form = $($target.closest('form'));
             var errors = $form.find('.errors');
+
             errors.removeClass('active');
 
-            $.ajax({
-                url: '/signin',
-                method: 'POST',
-                data: $form.serialize(),
-                success: function(data) {
+            signIn($form.serialize(), function(data) {
                     if (data._id) {
                         window.location = '/u/' + data._id;
                     } else {
                         errors.html('This shouldn\'t have happened. Sorry.');
                     }
-                },
-                error: function(res) {
+                }, function(res) {
                     console.log(res);
                     if (res.responseText == 'Unauthorized') {
                         errors.html('Please check your credentials.');
@@ -56,8 +69,7 @@
                     if (!errors.hasClass('active')) {
                         errors.addClass('active');
                     }
-                }
-            });
+                });
 
             // prevent sending of the form
             return false;
